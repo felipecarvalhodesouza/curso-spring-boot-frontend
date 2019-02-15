0b8143e483f65106b88a3d3c6b404365d9e67a6f
Felipe Carvalho de Souza <desouzafelipecarvalho@gmail.com>
2019-02-14 22:20:48 -0200

    Informando pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codPedido: string;
 
   constructor(
     public navCtrl: NavController,
@@ -55,7 +56,7 @@ export class OrderConfirmationPage {
   checkOut(){
     this.pedidoService.insert(this.pedido)
     .subscribe(response =>{
-      console.log(response.headers.get('location'));
+      this.codPedido = this.extractId(response.headers.get('location'));
       this.cartService.createOrClearCart();
     },
     error =>{
@@ -67,5 +68,14 @@ export class OrderConfirmationPage {
 
   back(){
     this.navCtrl.setRoot('CartPage');
+  }
+
+  home(){
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
+  private extractId(location: string) : string{
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
   }
 }
