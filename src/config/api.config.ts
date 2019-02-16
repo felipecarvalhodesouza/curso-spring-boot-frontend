11eddf7724f202bd67b91172c46beb96e865b0cd
Felipe Carvalho de Souza <desouzafelipecarvalho@gmail.com>
2019-02-16 16:09:35 -0200

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = { 
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://fatecbs-curso-spring.herokuapp.com",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/curso-spring-ionic-felipe"
 }
