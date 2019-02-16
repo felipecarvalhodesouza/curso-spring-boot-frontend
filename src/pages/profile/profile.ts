e373168a9f5d9548f52e0dd7649a0fb821e3c98b
Felipe Carvalho de Souza <desouzafelipecarvalho@gmail.com>
2019-02-16 18:36:25 -0200

    Bugfix: atualiação da imagem de perfil

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -5,6 +5,7 @@ import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { API_CONFIG } from '../../config/api.config';
 import { CameraOptions, Camera } from '@ionic-native/camera';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @IonicPage()
 @Component({
@@ -17,12 +18,16 @@ export class ProfilePage {
 
   picture: string;
   cameraOn: boolean = false;
+  profileImage;
 
   constructor(public navCtrl: NavController,
      public navParams: NavParams,
      public storage: StorageService,
      public clienteService: ClienteService,
-     public camera: Camera) {
+     public camera: Camera,
+     public sanitizer: DomSanitizer) {
+       
+      this.profileImage = 'assets/imgs/avatar-blank.png';
   }
 
   ionViewDidLoad() {
@@ -33,8 +38,23 @@ export class ProfilePage {
     this.clienteService.getImageFromBucket(this.cliente.id)
     .subscribe(response => {
       this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      this.blobToDataURL(response).then(dataUrl =>{
+        let str :string = dataUrl as string;
+        this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+      })
     },
-    error => {});
+    error => {
+      this.profileImage = 'assets/imgs/avatar-blank.png';
+    });
+  }
+
+  blobToDataURL(blob){
+    return new Promise((fulfill, reject) =>{
+      let reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = (e) => fulfill(reader.result);
+      reader.readAsDataURL(blob);
+    })
   }
 
   getCameraPicture(){
@@ -60,7 +80,7 @@ export class ProfilePage {
     this.clienteService.uploadPicture(this.picture)
     .subscribe(response => {
       this.picture = null;
-      this.loadData();
+      this.getImageIfExists();
     },
     error =>{
     });
